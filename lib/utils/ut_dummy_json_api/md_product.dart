529cb63b0d47dff28fffa569c5abbc2d6f3b2dc5
Thiago Sales <thiagobrunoms@gmail.com>
2024-05-10 09:57:02 -0300

    Update product model to handle to json mapping for Create and update operations

--- lib/utils/ut_dummy_json_api/md_product.dart
+++ lib/utils/ut_dummy_json_api/md_product.dart
@@ -58,4 +58,21 @@ class Product {
     thumbnail = json['thumbnail'];
     images = json['images'].cast<String>();
   }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+
+    data['title'] = title;
+    data['description'] = description;
+    data['price'] = price;
+    data['discountPercentage'] = discountPercentage;
+    data['rating'] = rating;
+    data['stock'] = stock;
+    data['brand'] = brand;
+    data['category'] = category;
+    data['thumbnail'] = thumbnail;
+    data['images'] = images;
+
+    return data;
+  }
 }
