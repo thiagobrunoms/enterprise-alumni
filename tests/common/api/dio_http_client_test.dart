3f2ba2e3601391b51a42ab0e68eaca4b9ab7184e
Thiago Sales <thiagobrunoms@gmail.com>
2024-05-12 23:30:14 -0300

    test: update http dio adapter tests

--- tests/common/api/dio_http_client_test.dart
+++ tests/common/api/dio_http_client_test.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-
-import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_template/common/api/dio_http_client.dart';
 import 'package:flutter_test_template/common/api/http_client.dart';
